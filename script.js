fafc6ef8a98a0cd8a2eedd37da00ee5779d103e4
Gustavo Miranda <96025432+gustamba@users.noreply.github.com>
2024-05-15 10:49:18 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let valores = []
 let numero = ''
-let limpar = false;
+let limpar = false
+let solucao = 0
 
 function adicionarValor(valor) {
     if(limpar){
@@ -14,7 +15,12 @@ function adicionarValor(valor) {
 
 function adicionarOperacao(operacao) {
     valores.push(numero.toString())
-    valores.push(operacao)
+    if (valores.length > 2) {
+        resultadoOperacao()
+        valores[0] = solucao;
+        delete valores[2]
+    }
+    valores[1] = operacao
     numero = ''
     console.log('Numero digitado: ' + valores[0])
     console.log('Operação: ' + valores[1]);
@@ -22,8 +28,34 @@ function adicionarOperacao(operacao) {
 }
 
 function resultado() {
-    valores.push(numero.toString())
-    let solucao = 0
+    valores[2] = numero.toString()
+    switch(valores[1]) {
+        case '+':
+            solucao = parseInt(valores[0]) + parseInt(valores[2]);
+            break
+        case '-':
+            solucao = parseInt(valores[0]) - parseInt(valores[2]);
+            break
+        case 'x':
+            solucao = parseInt(valores[0]) * parseInt(valores[2]);
+            break
+        case '/':
+            solucao = parseInt(valores[0]) / parseInt(valores[2]);
+            break
+        default:
+            solucao = 'Selecione uma operação';
+            break
+    }
+    console.log(valores, solucao);
+    let resultadoDisplay = document.getElementById("resultado");
+    resultadoDisplay.innerText = solucao;
+    numero = '';
+    valores = [];
+    limpar = true;
+}
+
+function resultadoOperacao() {
+    valores[2] = numero.toString()
     switch(valores[1]) {
         case '+':
             solucao = parseInt(valores[0]) + parseInt(valores[2]);
@@ -46,5 +78,4 @@ function resultado() {
     resultadoDisplay.innerText = solucao;
     numero = '';
     valores = [];
-    limpar = true;
 }
